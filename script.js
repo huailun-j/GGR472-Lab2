7ea2546e65f6db9c49f64ade1e20e6bf25296468
huailun-j <huailun.jiang@mail.utoronto.ca>
2025-02-06 23:50:56 -0500

    add

--- script.js
+++ script.js
@@ -7,10 +7,57 @@ const map = new mapboxgl.Map({
     zoom: 10, // starting zoom level
 });
 
+
+map.on('load', () => {
+    //add the first data source containing cycling network GeoJSON data
+    map.addSource('network-data', {
+        type: 'geojson',
+        data: 'https://huaijun-j.github.io/GGR472-Lab2/data/cycling-network.geojson' 
+
+    });
+
+     // Add a layer to display the cycling network lines
+     map.addLayer({
+        'id': 'network-line', // Unique ID for the layer
+        'type': 'line', // Type of layer (line)
+        'source': 'network-data', // Source of the layer data
+        'paint': {
+            'line-color': '#62a6f8', // Color of the lines
+            'line-width': 3, // Width of the lines
+            'line-opacity': 0.8 // Opacity of the lines
+        }
+    });
+    
+
+    //add the second data source containing parking racks GeoJSON data
+    map.addSource('parking-data', {
+        type: 'geojson',
+        data: 'https:///MissyZhang.github.io/GGR472-lab2/data/Bicycle%20Parking%20Racks%20Data.geojson' 
+
+    });
+    // Add a layer to display the parking rack points
+    map.addLayer({
+        'id': 'parking-point', // Unique ID for the layer
+        'type':'circle', // Type of layer (point)
+        'source': 'parking-data', // Source of the layer data
+        'paint': {
+            'circle-color': '#fdf6ba', // Color of the circles
+            'circle-opacity': 0.8, // Opacity of the circles
+            'circle-radius': 5 // Radius of the circles
+        }
+        
+    });
+
+    
+});
+
+
+
+
 map.on('load', () => {
     
     // Add a data source containing GeoJSON data
-    map.addSource('uoft-data', {
+    map.addSource('cyclying-network', {
         type: 'geojson',
         data: {
             "type": "FeatureCollection",
